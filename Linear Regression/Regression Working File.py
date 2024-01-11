cc2a1e717ef0234ac5f543206e0a1f828238cc02
Tishhshaw <tishtanyaa@icloud.com>
2024-01-11 13:21:22 -0500

    Refactored code into linear regression file, added K-Nearest Neighbors directory, added car data and split data into training and testing sets

--- Linear Regression/Regression Working File.py
+++ Linear Regression/Regression Working File.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 data = pd.read_csv("student-mat.csv", sep=";")
 
@@ -15,18 +18,27 @@ predict = "G3"
 
 X = np.array(data.drop([predict], axis=1))  # Returns a new df excluding "G3"
 Y = np.array(data[predict])
-
-# Split into 4 diff arrays
-# Splits 10% of data into test samples
 X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
 
-linear = linear_model.LinearRegression()
+"""
+best = 0
+for _ in range(30):
+    X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
 
-linear.fit(X_train, Y_train)  # Fits the daya to find a line of best fit
+    linear = linear_model.LinearRegression()
 
-linear.score(X_test, Y_test)  # Return a value that represents the accuracy of our model
-acc = linear.score(X_test, Y_test)
-print(acc)
+    linear.fit(X_train, Y_train)  # Fits the daya to find a line of best fit
+    acc = linear.score(X_test, Y_test)  # Return a value that represents the accuracy of our model
+    print(acc)
+
+    if acc > best:
+        best = acc
+        with open("student_performance_model.pickle", "wb") as f:
+            pickle.dump(linear, f)
+"""
+
+pickle_in = open("student_performance_model.pickle", "rb")
+linear = pickle.load(pickle_in)
 
 # Get y-intercept
 print("Co: \n ", linear.coef_)
@@ -37,3 +49,10 @@ predictions = linear.predict(X_test)
 
 for x in range(len(predictions)):
     print(predictions[x], X_test[x], Y_test[x])  # Prints prediction, input data, actual value of final grade
+
+p = "G1"
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()

5fecc5fd4e62e44d1a616b4440abae6de03a3adf
Tishtanya Shaw <tishtanyaa@icloud.com>
2024-01-12 15:35:43 -0500

    Initialized an SVM (Support Vector Machine) classifier using a linear kernel and set the regularization parameter 'C' to 2. Explored and compared the performance of a K-Nearest Neighbors Classifier with SVM to determine the optimal approach. Trained the SVM model on the training data and made predictions for evaluation.

--- Support Vector Machines/SVM Working File.py
+++ Support Vector Machines/SVM Working File.py
@@ -1,9 +1,11 @@
 import sklearn
 from sklearn import datasets
 from sklearn import svm
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
 
 cancer = datasets.load_breast_cancer()
-print(cancer)
+# print(cancer)
 
 # print(cancer.feature_names)
 # print(cancer.target_names)
@@ -14,7 +16,20 @@ y = cancer.target
 # Split the data into testing and taining sets
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
 
-print(x_train, y_train)
+# print(x_train, y_train)
+
+clf = svm.SVC(kernel="linear", C=2)
+
+# Test K-Nearest Neighbors Classifier
+# clf = KNeighborsClassifier(n_neighbors=9)
+
+clf.fit(x_train, y_train)
+
+y_pred = clf.predict(x_test)
+
+acc = metrics.accuracy_score(y_test, y_pred)
+
+print(acc)
 
 
 
